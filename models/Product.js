78417d8e7495b422afa42630432523046f6bcde5
William <will_cable@hotmail.com>
2023-02-23 09:55:55 +0800

    Finishing touches, all routes populated with correct query formatting

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,6 +11,37 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id : {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    product_name : {
+      type: DataTypes.STRING,
+      allowNull: false,
+      
+    },
+    price : {
+      type: DataTypes.DECIMAL(5,2),
+      allowNull: false,
+      // validate: {
+      //   Type: DataTypes.DECIMAL
+      // }
+    },
+    stock : {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      // validate: {
+      //   Type: DataTypes.INTEGER,
+      // }
+    },
+    category_id : {
+      type: DataTypes.INTEGER,
+      
+    }
+
   },
   {
     sequelize,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  const productData =  await Product.findAll({
+    include: [{model: Category}, {model: Tag}],
+  });
+
+  return res.json(productData);
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const productData = await Product.findByPk(req.params.id, {
+    include: [{model: Category}, {model: Tag}],
+  });
+
+  return res.json(productData);
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -48,7 +59,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -89,8 +100,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  await ProductTag.destroy({
+    where: {
+      product_id: req.params.id
+    }
+  })
+  
+  const productData = Product.destroy({where:
+      {
+      id: req.params.id
+      }
+    }
+  )
+
+  return res.json(productData);
+
 });
 
 module.exports = router;
